d579c4c4461032b071d2c8f9eb73c873f6ed6449
Maurice-Pascal Sonnemann <mpsonnemann@gmail.com>
2018-05-30 20:42:41 +0200

    Test out idea of r-tree pip in notebook

--- plotting/plotting/polyfill/pnpoly_rtree.hpp
+++ plotting/plotting/polyfill/pnpoly_rtree.hpp
@@ -1,4 +1,35 @@
 #ifndef PNPOLY_RTREE_HPP
 #define PNPOLY_RTREE_HPP
 
+#include <boost/geometry.hpp>
+#include <boost/geometry/geometries/point.hpp>
+#include <boost/geometry/geometries/box.hpp>
+#include <boost/geometry/index/rtree.hpp>
+
+namespace pip {
+namespace rtree {
+
+namespace bg = boost::geometry;
+namespace bgi = boost::geometry::index;
+
+using Point = bg::model::point<float, 2, bg::cs::cartesian>;
+using Box = bg::model::box<Point>;
+using Value = std::pair<Box, Segment>;
+
+class InclusionTester
+{
+public:
+    inline InclusionTester() {
+
+        //bgi::linear<16>, bgi::rstar<16>
+        bgi::rtree< value, bgi::quadratic<16> > rtree;
+
+    }
+    inline bool contains(Point const& point) {}
+    inline bool contains(Segment const& segment) {}
+};
+
+}
+} // namespace pnpoly
+
 #endif // PNPOLY_RTREE_HPP
